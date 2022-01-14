0b5d0f27a5093c46f733c1853c17ddb1bff77e16
yoonjoong <43026485+yoonjoong@users.noreply.github.com>
2022-01-14 16:47:02 +0900

    Update ch3_Q6.py

--- ch3_Q6.py
+++ ch3_Q6.py
@@ -1,19 +1,19 @@
 def solution(N, stages):
     clear = [0]*(N+2)
-    clear_dict ={}
+    fail_dict ={}
 
     for i in stages:
         clear[i] += 1
     
     for j in range(1,len(clear)-1):
         if (len(stages)-sum(clear[:j])) != 0 :
-            clear_dict[j] = clear[j]/(len(stages)-sum(clear[:j]))
+            fail_dict[j] = clear[j]/(len(stages)-sum(clear[:j]))
         else:  #분모가 0이 되는 경우 제외 (런타임 에러 발생)
-            clear_dict[j] = 0
+            fail_dict[j] = 0
 
-    clear_dict = sorted(clear_dict.items(), key=lambda x: (-x[1],x[0]))
+    fail_dict = sorted(fail_dict.items(), key=lambda x: (-x[1],x[0]))
     
-    answer = [n[0] for n in clear_dict]
+    answer = [n[0] for n in fail_dict]
     return answer
 
 N = 5
